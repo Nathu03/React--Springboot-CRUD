3152988543e48e8928a55757b68cd22f01ad0476
Narthanan <85630664+Nathu03@users.noreply.github.com>
2023-10-23 00:10:57 +0530

    code updated!

--- src/pages/ProductEdit.js
+++ src/pages/ProductEdit.js
@@ -15,7 +15,6 @@ function ProductEdit() {
     axios.get(`http://localhost:8080/api/products/${id}`)
       .then(response => {
         const product = response.data;
-  
         // Check if product data is valid before setting state
         if (product && product.name && product.price) {
           setName(product.name);
@@ -28,35 +27,46 @@ function ProductEdit() {
         console.error('Error fetching product data:', error);
       });
   }, [id]);
-  
 
+  
   const handleUpdate = (event) => {
     event.preventDefault();
-    axios.put(`http://localhost:8080/api/products/${id}`, { name, price })
-      .then(response => {
+    axios
+      .put(`http://localhost:8080/api/products/${id}`, { name, price })
+      .then((response) => {
         setSuccessMessage('Product updated successfully!');
         setTimeout(() => {
           setSuccessMessage('');
         }, 2000); // Hide success message after 2 seconds
-
+  
         console.log('Product updated successfully:', response.data);
         // Redirect to the product list page after successful update
-        navigate('/product-list');
+        navigate('/product-list', { state: { buttonClicked: true } });
       })
-      .catch(error => {
+      .catch((error) => {
         setErrorMessage('Error updating product. Please try again.');
         setTimeout(() => {
           setErrorMessage('');
         }, 2000); // Hide error message after 2 seconds
-
+  
         console.error('Error updating product:', error);
       });
   };
+  
 
   return (
     <div className="container mt-5">
       <h1 className="mb-4">Edit Product</h1>
-      {/* Your success and error messages go here */}
+      {successMessage && (
+        <div className="alert alert-success" role="alert">
+          {successMessage}
+        </div>
+      )}
+      {errorMessage && (
+        <div className="alert alert-danger" role="alert">
+          {errorMessage}
+        </div>
+      )}
       <form onSubmit={handleUpdate}>
         <div className="mb-3">
           <label htmlFor="productName" className="form-label">Name:</label>
@@ -65,7 +75,7 @@ function ProductEdit() {
             className="form-control"
             id="productName"
             value={name}
-            onChange={(e) => setName(e.target.value)} // Update name state when the input changes
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div className="mb-3">
@@ -75,17 +85,16 @@ function ProductEdit() {
             className="form-control"
             id="productPrice"
             value={price}
-            onChange={(e) => setPrice(e.target.value)} // Update price state when the input changes
+            onChange={(e) => setPrice(e.target.value)}
           />
         </div>
         <button type="submit" className="btn btn-primary me-2">Update Product</button>
-        {/* Add a cancel button */}
         <button type="button" className="btn btn-secondary" onClick={() => navigate('/product-list')}>
           Cancel
         </button>
       </form>
     </div>
   );
-};
+}
 
 export default ProductEdit;
